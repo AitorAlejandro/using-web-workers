c6ce6b772714562c9f54f33fc7cf122981b769dd
Aitor Alejandro Herrera <aitor.alejandro.herrera@gmail.com>
2018-05-24 10:15:17 +0200

    create demo1.js

---
+++ demos/demo1.js
@@ -0,0 +1,8 @@
+let lastIndex = 0;
+for (let index = 0; index < 3000000000; index++) {
+    // operación tonta:
+    const operacion1 = index / 0.33333;
+    const operacion2 = index / 0.66666;
+    lastIndex = index;
+}
+console.log('fin del bucle', lastIndex);
